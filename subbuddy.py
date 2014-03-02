8b12c0eefc2be6a7f25abad4a11348944febd85d
Ian Bradley <marlen.crabapple@gmail.com>
2014-03-01 17:52:22 -0800

    Subsync stuff

--- subbuddy.py
+++ subbuddy.py
@@ -25,32 +25,94 @@ yt_service.ssl = True
 yt_service.developer_key = dev_key
 new_subscription_videos_uri = 'https://gdata.youtube.com/feeds/api/users/default/newsubscriptionvideos'
 dldb = 'downloaded'
+subdb = 'localsubs'
 
 in_progress = []
 
 def main():
 	parser = argparse.ArgumentParser(description='YouTube subscription auto downloader command line options.')
 	parser.add_argument('-s', '--skip-current-queue', action='store_true')
+	parser.add_argument('-g', '--pull-subscriptions', action='store_true')
+	parser.add_argument('-p', '--push-subscriptions', action='store_true')
+	parser.add_argument('-I', '--run-once', action='store_true')
 	args = parser.parse_args()
+	login()
+	check_files()
 	
 	if queue_size != 25:
 		if queue_size <= 50 or queue_size >= 1:
 			global new_subscription_videos_uri
 			new_subscription_videos_uri += '?max-results=' + str(queue_size)
 	
-	login()
+	if args.pull_subscriptions:
+		print 'Grabbing remote subscriptions.'
+		pull_subscribed_to();
+	
+	if args.push_subscriptions:
+		print 'Adding local subscriptions.'
+		push_subscribed_to();
 	
 	if args.skip_current_queue:
 		print 'Skipping current subscription queue.'
 		skip_current_queue();
+		
+	if args.run_once:
+		print 'Goodbye.'
+		sys.exit(0)
 	
 	while True:
-		if not os.path.exists(dldb):
-			file(dldb, 'w').close()
-			
+		check_files()
 		check_and_download_subscriptions()
 		print 'Waiting', refresh_rate, 'seconds...';
 		time.sleep(refresh_rate)
+		
+def check_files():
+	if not os.path.exists(dldb):
+		file(dldb, 'w').close()
+	
+	if not os.path.exists(subdb):
+		file(subdb, 'w').close()
+
+def pull_subscribed_to():
+	subscription_feed = yt_service.GetYouTubeSubscriptionFeed('https://gdata.youtube.com/feeds/api/users/default/subscriptions?max-results=50')
+	subscribed_to = [line.strip() for line in open(subdb)]
+	total_subscriptions = int(subscription_feed.total_results.text)
+	total_pages = total_subscriptions / 50
+	
+	for x in range(0, total_pages):
+		if x > 0:
+			try:
+				subscription_feed = yt_service.GetYouTubeSubscriptionFeed('https://gdata.youtube.com/feeds/api/users/default/subscriptions?max-results=50&start-index=' + str(x * 50))
+			except gdata.service.RequestError:
+				return
+				
+		for entry in subscription_feed.entry:
+			if entry.username.text not in subscribed_to:
+				f = open(subdb,'a')
+				f.write(entry.username.text + '\n')
+				f.close()
+
+def push_subscribed_to():
+	subscription_feed = yt_service.GetYouTubeVideoFeed('https://gdata.youtube.com/feeds/api/users/default/subscriptions?max-results=50')
+	subscribed_to_local = [line.strip() for line in open(subdb)]
+	subscribed_to_remote = []
+	total_subscriptions = int(subscription_feed.total_results.text)
+	total_pages = total_subscriptions / 50
+		
+	for x in range(0, total_pages):
+		if x > 0:
+			try:
+				subscription_feed = yt_service.GetYouTubeSubscriptionFeed('https://gdata.youtube.com/feeds/api/users/default/subscriptions?max-results=50&start-index=' + str(x * 50))
+			except gdata.service.RequestError:
+				break
+				
+		for entry in subscription_feed.entry:
+			subscribed_to_remote.append(entry.username.text)
+		
+	for local_sub in subscribed_to_local:
+		if local_sub not in subscribed_to_remote:
+			new_subscription = yt_service.AddSubscriptionToChannel(
+				username_to_subscribe_to=local_sub)
 		
 def skip_current_queue():
 	ids = get_feed(new_subscription_videos_uri)
